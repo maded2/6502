2d26f916a180e16714df1a83724d151e6e9865c8
Eddie <eddie@pobox.com>
2019-11-20 23:29:27 +0800

    add address decoding

--- src/decoder/main.go
+++ src/decoder/main.go
@@ -9,21 +9,42 @@ import (
 	"os"
 )
 
+/*
+							FROM ADDR						TO ADDR						CHIP SELECT VALUE		ACTIVE
+0000-7EFF		RAM			0000 0000 0000 000	0000		0111 1110 1111 111	3F7F		0000 0001				0		$01
+7F00-7FFF		I/O
+	7F00-7F0F	VIA1		0111 1111 0000 000	3F80		0111 1111 0000 111	3F87		1100 0000				01		$C0
+	7F10-7F1F	VIA2		0111 1111 0001 000	3F88		0111 1111 0001 111	3F8F		0011 0000				01		$30
+8000-FFFF		ROM			1000 0000 0000 000	4000		1111 1111 1111 111	7FFF		0000 0010				0		$02
+
+*/
 func main() {
+
+	var a []byte
+	a = make([]byte, 32768)
+
+	setArea(a, "0000", "3F7F", '\x01')
+	setArea(a, "3F80", "3F87", '\xC0')
+	setArea(a, "3F88", "3F8F", '\x30')
+	setArea(a, "4000", "7FFF", '\x02')
+
+	writeMap(a)
+}
+
+func setArea(a []byte, start, end string, value byte) {
+	s := hex2i(start)
+	e := hex2i(end)
+	for i := s; i <= e; i++ {
+		a[i] = value
+	}
+}
+
+func writeMap(a []byte) {
 	file, err := os.Create("decode.bin")
 	if err != nil {
 		log.Fatalf("Failed to create decode.bin file: %s", err)
 	}
-
-	var a []byte
-	a = make([]byte, 32768)
 	w := bufio.NewWriter(file)
-	for i := 0; i < 32768; i++ {
-		a[i] = '\xEA'
-	}
-	a[hex2i("7ffc")] = '\x00'
-	a[hex2i("7ffd")] = '\x80'
-
 	if _, err := w.Write(a); err != nil {
 		log.Fatal(err)
 	}
@@ -33,7 +54,7 @@ func main() {
 	if err := file.Close(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("hello world")
+	fmt.Println("done")
 }
 
 func hex2i(s string) uint16 {
@@ -41,7 +62,7 @@ func hex2i(s string) uint16 {
 		return 0
 	} else {
 		v := binary.BigEndian.Uint16(b)
-		fmt.Println(v)
+		//fmt.Println(v)
 		return v
 	}
 }
