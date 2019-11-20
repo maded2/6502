a50225f7b1b0b4d0c769c3dead66ac3a8b041add
Eddie <eddie@pobox.com>
2019-11-20 22:42:22 +0800

    first rom code

---
+++ src/rom1/test.asm
@@ -0,0 +1,38 @@
+; vasm -Fbin -dotdir src/rom1/test.asm
+; minipro -p AT28C256 -w a.out
+
+    .org $8000
+reset:
+
+    lda #$50
+    sta $6000
+
+loop:
+    ror
+    sta $6000
+
+
+        ldy  #$ff   ; (2 cycles)
+delay2: ldx  #$ff   ; (2 cycles)
+delay:  nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        nop
+        dex          ; (2 cycles)
+        bne  delay   ; (3 cycles in loop, 2 cycles at end)
+        dey          ; (2 cycles)
+        bne  delay2   ; (3 cycles in loop, 2 cycles at end)
+
+
+    jmp loop
+
+    .org $fffc
+    .word reset
+    .word reset
